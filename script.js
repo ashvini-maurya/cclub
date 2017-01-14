205f062346fadacc7ddcd10e5f5379846d8ca5ed
ads <abhijeet3192@gmail.com>
2017-01-14 14:46:35 +0530

    Corrected js requests

--- script.js
+++ script.js
@@ -1,29 +1,24 @@
 $(document).ready(function() {
 
-	object2 = {Frontend: [{name: "CSS", url: "css.com", data: "markdown text"}, 
-			  			  {name: "JavaScript", url: "javascript.com"},
-			  			  {name: "FontAwsome", url: "fontawsome.com"}],
+    url = 'http://localhost:3000/'
 
-			   Linux: [{name: "Bash", url: "tldp.com"},
-			   		   {name: "Basic servers", url: "server.com"}],
+    init = ''
+    $.getJSON(url + 'init', function(data){ 
+    	Object.keys(data).map(function(tech){
+            $('#syllabus').append('<li data-toggle="collapse" data-target="#' + tech +'" class="collapsed">\
+                        <a href="#"><i class="fa fa-lg ">' +  tech +'</i></a>\
+                        <ul class="sub-menu collapse ' +  tech + '" id="' + tech + '"></ul>\
+            ')
 
-			   Backend: [{name: "DataBase", url: "db.com"}, 
-			   			 {name: "Servers", url: "aws.com"}]
-			   }
+            data[tech].map(function(value) {
+                $('.' + tech).append('<li class="subheading" data-url=' + value.url + '>' + value.name + '</li>');
+            });
 
-	Object.keys(object2).map(function(tech){
-		$('#syllabus').append('<li data-toggle="collapse" data-target="#' + tech +'" class="collapsed">\
-					<a href="#"><i class="fa fa-lg ">' +  tech +'</i></a>\
-					<ul class="sub-menu collapse ' +  tech + '" id="' + tech + '"></ul>\
-		')
-
-		object2[tech].map(function(value) {
-			$('.' + tech).append('<li class="subheading" data-url=' + value.url + '>' + value.name + '</li>');
-		});
-
-	});
-
-    $('.subheading').on('click', function(){
-        $('#lesson').html("marked data will go here.");
-    })
+            $('.subheading').on('click', function(){
+                url = $(this).data('url');
+                $('#lesson').html("marked data will go here.");
+            })
+	    });
+    });
 });
+
