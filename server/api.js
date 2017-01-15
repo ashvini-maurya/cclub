4de1249d74b0b3d60da4256e01f19fe10b4d6fd8
cancerian champ <harshit.chaudhary@y7mail.com>
2017-01-15 16:43:06 +0530

    user get by username is done

---
+++ server/api.js
@@ -0,0 +1,32 @@
+
+/**
+ * This file manages the initial api server calls 
+ * @author Harshit Chaudhary <harshit.chaudhary@y7mail.com>
+ * @version 1.0
+ */
+
+var express = require('express');
+var status = require('http-status');
+
+module.exports = function(wagner) {
+	var api = express.Router();
+
+	api.get('/user/:username', wagner.invoke(function(User){
+		return function(req, res) {
+			User.findOne({username: req.params.username}, function(error, user) {
+				if(error) {
+					return res.
+					status(status.INTERNAL_SERVER_ERROR).
+					json({error: error.toString()});
+				}
+				if(!user) {
+					return res.
+					status(status.NOT_FOUND).
+					json({error: 'Not Found'});
+				}
+			});
+		};
+	}));
+	
+	return api;
+}
---
+++ server/index.js
@@ -0,0 +1,18 @@
+
+/** 
+ * This is basic init file that will bootstrap the application server
+ * @author Harshit Chaudhary <harshit.chaudhary@y7mail.com>
+ * @version 1.0
+ */
+
+ var express = require('express');
+ var wagner = require('wagner-core');
+ 
+ require('./models')(wagner);
+
+ var app = express();
+ 
+ app.use('/api/v1', require('./api')(wagner));
+
+ app.listen(3000);
+ console.log('Listening on port 3000');
