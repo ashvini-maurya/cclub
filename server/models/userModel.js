e84bd41faa40f23e5b7884b799dfcc81020de691
Abhijeet De <abhijeet3192@gmail.com>
2017-01-21 14:40:55 +0530

    Maintaining the es6 standard for other js files

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,21 +1,21 @@
 /**
  * This is the mongoose schema design for User Model/Schema
- * @author Harshit Chaudhary <harshit.chaudhary@y7mail.com>	 
+ * @author Harshit Chaudhary <harshit.chaudhary@y7mail.com>
  * @version 1.0
  */
 
- var mongoose = require('mongoose');
- var userSchema = {
-		username: { type: String, required: true, lowercase: true },
-		email: {type: String, required: true, match: /.+@.+\..+/, lowercase: true},
-		password: {type: String, required: true},
-		full_name: {type: String, required: true },
-		role: {type: String, required: true},
-		levels: [Object],
-		last_login: {type: Number}
-	};
+const mongoose = require('mongoose');
 
- module.exports = new mongoose.Schema(userSchema);
- module.exports.userSchema = userSchema;
+const userSchema = {
+  username: { type: String, required: true, lowercase: true },
+  email: { type: String, required: true, match: /.+@.+\..+/, lowercase: true },
+  password: { type: String, required: true },
+  full_name: { type: String, required: true },
+  role: { type: String, required: true },
+  levels: [Object],
+  last_login: { type: Number },
+};
 
+module.exports = new mongoose.Schema(userSchema);
+module.exports.userSchema = userSchema;
 
--- server/routes/users/index.js
+++ server/routes/users/index.js
@@ -1,27 +1,22 @@
-
 /**
- * This file manages the initial api server calls 
+ * This file manages the initial api server calls
  * @author Harshit Chaudhary <harshit.chaudhary@y7mail.com>
  * @version 1.0
  */
 
-var users = require('express').Router();
-var status = require('http-status');
+const users = require('express').Router();
+const status = require('http-status');
 
-users.get('/user/:username', function(req, res) {
-    User.findOne({username: req.params.username}, function(error, user) {
-        if(error) {
-            return res.
-            status(status.INTERNAL_SERVER_ERROR).
-            json({error: error.toString()});
-        }
-        if(!user) {
-            return res.
-            status(status.NOT_FOUND).
-            json({error: 'Not Found'});
-        }
-        res.json({user: user});
-    });
+users.get('/user/:username', (req, res) => {
+  User.findOne({ username: req.params.username }, (error, user) => {
+    if (error) {
+      return res.status(status.INTERNAL_SERVER_ERROR).json({ error: error.toString() });
+    }
+    if (!user) {
+      return res.status(status.NOT_FOUND).json({ error: 'Not Found' });
+    }
+    return res.json({ user });
+  });
 });
 
 module.exports = users;
