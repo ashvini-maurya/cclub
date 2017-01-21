b4e0943b131d24fbaf473a6157fad3fa8d8f9f0b
Abhijeet De <abhijeet3192@gmail.com>
2017-01-21 14:22:10 +0530

    eslintrc correction + index.js made to es6

--- server/index.js
+++ server/index.js
@@ -1,43 +1,37 @@
 
-/** 
+/**
  * This is basic init file that will bootstrap the application server
  * @author Harshit Chaudhary <harshit.chaudhary@y7mail.com>
  * @version 1.0
  */
 
-var express = require('express');
-var http = require('http').Server(app);
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-var path = require('path');
-var routes = require('./routes');
-var db;
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const path = require('path');
+const routes = require('./routes');
 
-if (process.env.ENV == 'production') {
-    db = mongoose.connect(''); 
+if (process.env.ENV === 'production') {
+    let db = mongoose.connect('');
 } else {
-    db = mongoose.connect('mongodb://localhost/c_club');
+    let db = mongoose.connect('mongodb://localhost/c_club');
 }
 
-var app = express();
+const app = express();
 
 require('./models');
+
 app.use('/', routes);
 
-var port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var app = express();
+app.use(express.static(path.resolve('./../public')));
 
-app.use(express.static(path.resolve(__dirname + '/../public')));
- 
 app.use('/api/v1', require('./routes'));
-app.get('/', function(req, res){
-    res.sendFile(path.resolve(__dirname + '/../public/index.html'));
-});
 
-app.listen(port, function(){
-    console.log('Listening on port 3000');
-});
+app.get('/', (req, res) => res.sendFile(path.resolve('./../public/index.html')));
+
+app.listen(port, () => console.log('Listening on port 3000'));
